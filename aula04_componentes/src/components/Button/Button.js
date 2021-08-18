e8fd01e572328a4d69ed9dc9d15f483d2ed0af43
Andressa Oliveira <C0614396@vale.com>
2021-08-17 23:01:31 -0300

    new itens

---
+++ aula04_componentes/src/components/Button/Button.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Button.css'
+
+export class Button extends React.Component {
+
+    click() {
+        console.log('clicou no botão')
+    }
+
+    render() {
+        return (
+            <div className="button-component">
+                <div onClick={this.props.onClick ?? (() => this.click())}>{this.props.title}</div>
+            </div >
+        )
+    }
+}
+
+Button.defaultProps = {
+    title: 'Botão',
+};
+
