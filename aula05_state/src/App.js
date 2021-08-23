a7b1c5e7a15bd082078d5411177420503232a1e7
Andressa Oliveira <C0614396@vale.com>
2021-08-23 20:51:53 -0300

    att

--- aula05_state/src/App.js
+++ aula05_state/src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
 
-export default class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { name: '' };
-    }
-  
-    changeName(newName) {
-      this.setState({ name: newName });
-    }
-  
-    render() {
-      return (
-          <div>
-            <h1>Meu nome é: {this.state.name}</h1>
-            <input
-              onChange={(e) => this.changeName(e.target.value)}
-            />
-          </div>
-      )
-    }
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      showName: false
+    };
   }
-  
+
+  changeName(newName) {
+    this.setState({ name: newName });
+  }
+
+  changeShowName() {
+    this.setState({ showName: !this.state.showName })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.showName ?
+          <h1>Meu nome é: {this.state.name}</h1>
+          : null}
+
+        <input
+          onChange={(e) => this.changeName(e.target.value)}
+        />
+
+        <button onClick={() => this.changeShowName()}>
+          {this.state.showName ? 'Ocultar Nome' : 'Mostrar Nome'}
+        </button>
+      </div>
+    )
+  }
+}
