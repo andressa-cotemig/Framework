3c117722b7d61f238c9d231a8b43d7feaf2ba072
Andressa Oliveira <C0614396@vale.com>
2021-09-27 14:55:58 -0300

    att

--- src/infra/store.js
+++ src/infra/store.js
@@ -7,3 +7,7 @@ import { Reducers } from './reducers/index';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 export const Store = createStore(Reducers, composeWithDevTools());
+
+Store.subscribe(() =>
+  console.log('store foi modificada', Store.getState())
+)
