198faa60faed52e6a3471d8e9ebd6f1b377af227
Andressa Oliveira <C0614396@vale.com>
2021-09-27 22:20:27 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 //import './App.css';
 
-import { Aula11 } from "./aula11_redux";
-
 // import Aula01 from "./aula01_elementos/Aula01";
 // import Aula02 from "./aula02_lista-itens/Aula02";
 // import Aula03 from "./aula03_bootstrap/Aula03";
@@ -10,8 +8,7 @@ import { Aula11 } from "./aula11_redux";
 // import Aula06 from "./aula06_imc/Aula06";
 // import Aula07 from "./aula07_lifecycle/Aula07";
 // import Aula08 from "./aula08_sass/Aula08";
-// import Aula09 from "./aula09_consumir-api/Aula09";
-// import Aula10 from "./aula10_consumir-api-axios/Aula10";
+import Aula09 from "./aula09_consumir-api/Aula09";
 
 //  npm i -g create-react-app           instalação
 //  npx create-react-app my-app         criar novo projeto
@@ -19,12 +16,10 @@ import { Aula11 } from "./aula11_redux";
 function App() {
   return (
     <div>
-      <Aula11/>
+      <Aula09/>
       {/*
-      <Aula10 />
       <Aula09 />
       <Aula08 />
-      <Aula07 />
       <Aula07 />
       <Aula06 />
       <Aula05 />
